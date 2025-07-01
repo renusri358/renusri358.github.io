2c0ca1767b85eea42e71932af1bd420f1d851a1e
renusri358 <renusri.ad23@krct.ac.in>
2025-07-01 23:04:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function showMessage() {
+  alert("Thank you for exploring my portfolio, Sri! 🎉\nWishing you success and smiles!");
+}
+
+function toggleDesc(id) {
+  const desc = document.getElementById(id);
+  desc.style.display = (desc.style.display === "none" || desc.style.display === "") ? "block" : "none";
+}
+
+
+
+
+
